2cb10055b34972644d705bb07f80a0d40ac85002
Oleg Morozenkov <m@oleg.rocks>
2018-03-03 00:02:39 +0300

    Fix error 'unorderable types: int() > NoneType()'

--- vk_channelify/models/channel.py
+++ vk_channelify/models/channel.py
@@ -8,7 +8,7 @@ class Channel(Base):
 
     channel_id = Column(String, primary_key=True, nullable=False)
     vk_group_id = Column(String, nullable=False)
-    last_vk_post_id = Column(Integer, nullable=False, server_default='0')
+    last_vk_post_id = Column(Integer, nullable=False, server_default='0', default=0)
     owner_id = Column(String, nullable=False)
     owner_username = Column(String)
     hashtag_filter = Column(String)
